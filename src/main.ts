e3485a97535283f0c3816619563d76c314a62da6
Rowan-Paul <contact@rowanpaulflynn.com>
2025-01-01 15:30:56 +0100

    feat: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenWatch API')
+    .setDescription('The OpenWatch API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
