2d6c078e284759ef24750e509c24813cd795adf8
Rowan-Paul <rowan-paul@webbio.nl>
2025-01-01 21:53:29 +0100

    feat: Add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,32 @@ import { MoviesModule } from './movies/movies.module';
 import { StreamingServicesModule } from './streaming-services/streaming-services.module';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from '@anatine/zod-nestjs';
 import { ApplicationsModule } from './applications/applications.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, MoviesModule, StreamingServicesModule, ApplicationsModule],
+  imports: [
+    AuthModule,
+    MoviesModule,
+    StreamingServicesModule,
+    ApplicationsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10000,
+        limit: 10,
+      },
+    ]),
+  ],
   providers: [
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
     PrismaService,
   ],
