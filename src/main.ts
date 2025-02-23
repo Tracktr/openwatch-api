892337982109ae19e10b05006e51d4bfc8b2308b
Rowan-Paul <contact@rowanpaulflynn.com>
2025-02-23 15:44:40 +0100

    chore: Remove swagger prefix

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .build();
   patchNestjsSwagger();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   // passport doesn't work nicely with fastify, so we need to add some hooks to make it work
   // github.com/nestjs/nest/issues/5702
